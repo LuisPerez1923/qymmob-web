641c02660cc437ceb45c03cbce0ed7e7d5516853
AlmaDividida <david_626180497@hotmail.com>
2020-08-12 21:34:19 -0500

    Se corrigieron efectos JQuery

--- js/app.js
+++ js/app.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
 
 $(window).scroll(function() {
     $(".barra").css("transition", ".4s");
-    $(".bg-header").css("transition", "1.5s");
+    $(".bg-header").css("transition", ".6s");
     if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-        $(".barra").css("background", "#222222");
+        $(".barra").css("background", "#2b2b2b");
         $(".bg-header").css("opacity", "0.05");
     } else {
         $(".barra").css("background", "none");
