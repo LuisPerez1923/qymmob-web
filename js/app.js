029c33e8be4935924ea3f95903c131afa1ab8600
AlmaDividida <david_626180497@hotmail.com>
2020-08-12 21:22:35 -0500

    Se agregaron animaciones JQuery para el Header

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/* ANIMACION DEL NAVBAR AL HACER CLICK EN EL MENU HAMBURGUESA */
+
+$(document).ready(function() {
+    $(".navbar").hide();
+    $("#menu-hamb").click(function() {
+        $(".navbar").toggle("linear");
+    });
+});
+
+/* ANIMACION DEL HEADER AL HACER SCROLL HACIA ABAJO */
+
+$(window).scroll(function() {
+    $(".barra").css("transition", ".4s");
+    $(".bg-header").css("transition", "1.5s");
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        $(".barra").css("background", "#222222");
+        $(".bg-header").css("opacity", "0.05");
+    } else {
+        $(".barra").css("background", "none");
+        $(".bg-header").css("opacity", "1");
+    }
+});
